ac1ffca203fea03a98ae657f8ea05b6eedd41526
User <suharmaksim@gmail.com>
2024-09-21 13:06:43 +0300

    youtube end to end test

---
+++ src/test/java/YouTubeTest.java
@@ -0,0 +1,33 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.BaseFixture;
+
+import static com.codeborne.selenide.Selenide.closeWindow;
+
+public class YouTubeTests extends BaseFixture {
+
+    @BeforeClass(description = "Open YouTube page, maximaze browser window")
+    void setUp() {
+        openYouTube();
+    }
+
+    @Test(description = "YouTube page end to end test")
+    public void youTubeSearchTest() {
+        pages.youTubePage.isOpened()
+                .enterRandomNumbersInSearch()
+                .selectDropdownItem(1)
+                .clickOnVideo(3)
+                .checkVideoIsOpened()
+                .clickOnOwnerAvatar()
+                .isOpened()
+                .clickSubscribeBtn()
+                .checkSignInText();
+    }
+
+    @AfterClass
+    private void tearDown() {
+        closeWindow();
+    }
+}
+
